07c9150a70c63758d9e7fb6c38436a667c36dc3b
EtienneCaus <caussinusetienne@yahoo.com>
2024-12-23 17:03:32 -0500

    Added Torch

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -15,13 +15,15 @@ public partial class Player : CharacterBody3D
 	public const float BobAmplitude = 0.04f;
 	float timeBob = 0f;
 	public Node3D head;
-	Camera3D camera;
+	Camera3D camera, weaponCam;
 	SpotLight3D spotLight;
 
     public override void _Ready()
     {
 		head = GetNode<Node3D>("Head");	//Set the head and camera
 		camera = GetNode<Camera3D>("Head/Camera3D");
+		weaponCam = GetNode<Camera3D>("Head/Camera3D/SubViewportContainer/SubViewport/Camera3D");
+		GetNode<SubViewport>("Head/Camera3D/SubViewportContainer/SubViewport").Size = DisplayServer.WindowGetSize();
 		spotLight = GetNode<SpotLight3D>("Head/SpotLight3D");
 		Input.MouseMode = Input.MouseModeEnum.Captured; //Capture the mouse
         base._Ready();
@@ -64,7 +66,18 @@ public partial class Player : CharacterBody3D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
+		Vector2 CameraDir = Input.GetVector("lookleft", "lookright", "lookup", "lookdown");
 		Vector3 direction = (head.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+
+
+		//Moves the Camera
+		head.RotateY(-CameraDir.X * Sensitivity * 20);
+		camera.RotateX(-CameraDir.Y * Sensitivity * 10);	//Bouge la caméra du joueur et empêche de dépasser le 90°
+		camera.Rotation = new Vector3(Clamp(camera.Rotation.X, DegToRad(-90), DegToRad(90)), camera.Rotation.Y, camera.Rotation.Z);
+		spotLight.Rotation = camera.Rotation;
+
+		//weaponCam.GlobalTransform = camera.GlobalTransform;		//Weapon camera
+		GetNode<SubViewport>("Head/Camera3D/SubViewportContainer/SubViewport").Size = DisplayServer.WindowGetSize();
 
 		if(IsOnFloor())
 		{
@@ -92,6 +105,11 @@ public partial class Player : CharacterBody3D
 		transBob.Origin = HeadBob(timeBob);	//Perform Headbobs
 		camera.Transform = transBob;	//Change the Origin of the Camera
 
+		MeshInstance3D torch = GetNode<MeshInstance3D>("Head/Camera3D/SubViewportContainer/SubViewport/Camera3D/Torch");
+		Transform3D transWeapon = torch.Transform;
+		transWeapon.Origin = WeaponBob(timeBob);
+		torch.Transform = transWeapon;
+
 		Velocity = velocity;
 		MoveAndSlide();
 	}
@@ -106,6 +124,18 @@ public partial class Player : CharacterBody3D
 
 		return position;
 	}
+	Vector3 WeaponBob(float time)
+	{
+		Vector3 position = Vector3.Zero;
+
+		//Moves the head in a sine wave, ie. up and down
+		position.Y = Sin(time * BobFrequency) * (BobAmplitude / 2) - 0.2f;
+		position.X = Cos(time * BobFrequency / 2) * (BobAmplitude / 2) + 0.2f;
+		position.Z = -0.25f;
+
+		return position;
+	}
+
 
 	public void SetPosition(Vector2I tile)
 	{
